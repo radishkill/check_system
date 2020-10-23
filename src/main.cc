8c6326c81c2e5a4678317c2a14414ed04fdec7b3
zenqiang <1261729204@qq.com>
2020-10-23 15:43:21 -0700

    modify

--- src/main.cc
+++ src/main.cc
@@ -3,32 +3,26 @@
 #include <fstream>
 #include <unistd.h>
 
-#include "global_arg.h"
 #include "usart.h"
 #include "laser.h"
-#include "camera_manager.h"
-#include "utils.h"
 
+#include"camera_manager.h"
 
 void InitSystem() {
-  GlobalArg* arg = GlobalArg::GetInstance();
-  arg->laser = new Laser("/dev/ttyUSB0");
-  arg->camera = new CameraManager();
+  Laser laser("/dev/ttyUSB0");
+  laser.SendCloseCmd();
+  sleep(5);
+  laser.SendOpenCmd();
+  sleep(20);
+  laser.SendCheckCmd();
+  sleep(20);
+  laser.SendCloseCmd();
 }
-
-void SelfTest() {
-  GlobalArg* arg = GlobalArg::GetInstance();
-  arg->laser->SendCloseCmd();
-  Utils::MSleep(3000);
-  arg->laser->SendOpenCmd();
-  Utils::MSleep(3000);
-  arg->camera->GetPic();
-  arg->camera->CheckPic();
-  arg->laser->SendCloseCmd();
+void InitCamera() {
+  CameraManager();
 }
-
-
 int main() {
   InitSystem();
+  InitCamera();
   return 0;
 }
