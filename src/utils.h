3c37c3b39b0f1154f043ba0012243cb805f49631
zenqiang <1261729204@qq.com>
2020-10-25 15:32:31 -0700

    AppendPufFile(); AppendSeed(); Create files and folders

--- src/utils.h
+++ src/utils.h
@@ -2,12 +2,13 @@
 #define UTILS_H
 
 #include <linux/types.h>
-
+#include<iostream>
 
 class Utils {
   public:
   static unsigned char CheckSum(unsigned char* p, int datalen);
   static int MSleep(unsigned int ms, bool force_sleep = true);
+  static std::string DecToStr(int para,int w);
 };
 
 
